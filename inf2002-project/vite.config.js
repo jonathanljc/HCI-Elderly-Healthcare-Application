2d61d5a225a674f451d8655ada32dfa20dbf3bde
liangzhi61 <2300948@sit.singaporetech.edu.sg>
2024-10-31 17:17:04 +0800

    Integrated with phone
    Libraries needed is installed and make changes to the app logo

--- inf2002-project/vite.config.js
+++ inf2002-project/vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      manifest: {
+        name: 'Elderhealth',
+        short_name: 'App',
+        description: 'An app to enhance convernience for you',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
